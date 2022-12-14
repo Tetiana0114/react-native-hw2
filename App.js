556721bf7f45e10aa71ea1aab66d09b092cbfedd
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-12-15 01:14:16 +0200

    Add registration form

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+import RegistrationScreen from './Screens/RegistrationScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RegistrationScreen/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,138 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
+const initialState = {
+  login: "",
+  email: "",
+  password: "",
+};
+
+export default function RegistrationScreen() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+
+    const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+    };
+
+  return (
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={keyboardHide}>
+      <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"}>
+      <View style={{ ...styles.form, marginBottom: isShowKeyboard ? 32 : 66 }}>
+      <Text style={styles.formTitle}>Registration</Text>
+        <View>
+        <TextInput
+        style={styles.input}
+        textAlign={"center"}
+        placeholder="Login"
+        onFocus={() => setIsShowKeyboard(true)}
+        value={state.login}
+        onChangeText={(value) =>
+        setState((prev) => ({ ...prev, login: value }))
+        }
+        />
+        </View>
+   
+        <View>
+        <TextInput
+        style={styles.input}
+        textAlign={"center"}
+        placeholder="Email"
+        onFocus={() => setIsShowKeyboard(true)}
+        value={state.email}
+        onChangeText={(value) =>
+        setState((prev) => ({ ...prev, email: value }))
+        }
+        />
+        </View>
+      
+        <View>
+        <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        textAlign={"center"}
+        placeholder="Password"
+        onFocus={() => setIsShowKeyboard(true)}
+        value={state.password}
+        onChangeText={(value) =>
+        setState((prev) => ({ ...prev, password: value }))
+        }
+        />
+        </View>
+         <TouchableOpacity
+              activeOpacity={0.8}
+              style={styles.btn}
+              onPress={keyboardHide}
+        >
+        <Text style={styles.btnName}>Register</Text>
+        </TouchableOpacity>
+        <Text style={styles.bottomText}>Do you already have an account? Log In</Text>
+      
+        </View>
+        </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
+    borderRadius: 8,
+    marginBottom: 16,
+    height: 50,
+  },
+  form: {
+    // marginHorizontal: 16,
+    width: 343,
+  },
+  formTitle: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#212121",
+    marginBottom: 33,
+  },
+  btn: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FF6C00",
+    marginTop: 27,
+    marginBottom: 16,
+    borderRadius: 100,
+    height: 50,
+  },
+  btnName: {
+    fontSize: 16,
+    color: "#FFFFFF",   
+  },
+  bottomText: {
+    fontSize: 16,
+    color: "#1B4371",
+    textAlign: "center",
+  },
+});
