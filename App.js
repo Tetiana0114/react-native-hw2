0f98a165862fed84d5039ab0416f416ecfb9ea28
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-12-21 01:02:07 +0200

    hw-4/add bottom navigation

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from "@react-navigation/native";
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React, { useCallback, useEffect, useState } from 'react';
-import {} from "react-native";
+import { } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 // import * as Font from 'expo-font';
 // import * as SplashScreen from 'expo-splash-screen';
 
-
 import RegistrationScreen  from './Screens/auth/RegistrationScreen';
 import LoginScreen from './Screens/auth/LoginScreen';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import PostsScreen from './Screens/mainScreen/PostsScreen';
+import CreatePostsScreen from './Screens/mainScreen/CreatePostsScreen';
+import ProfileScreen from './Screens/mainScreen/ProfileScreen';
 
 // SplashScreen.preventAutoHideAsync();
 const AuthStack = createNativeStackNavigator();
+const MainTab = createBottomTabNavigator();
 
+const useRoute = (isAuth) => {
+  if (!isAuth) {
+    return (
+      <AuthStack.Navigator>
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Login"
+          component={LoginScreen} />
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Register"
+          component={RegistrationScreen} />
+      </AuthStack.Navigator>
+    );
+  }
+  return (
+    <MainTab.Navigator>
+        <MainTab.Screen name="Posts" component={PostsScreen} />
+        <MainTab.Screen name="Create" component={CreatePostsScreen} />
+        <MainTab.Screen name="Profile" component={ProfileScreen} />
+    </MainTab.Navigator>
+  );
+};
 
 export default function App() {
 //   const [appIsReady, setAppIsReady] = useState(false);
@@ -47,24 +76,13 @@ export default function App() {
 //     return null;
 //   }
   
+const routing = useRoute({});
+// const routing = useRoute(null);
+  
   return (
-<NavigationContainer>
-      <AuthStack.Navigator>
-        <AuthStack.Screen
-          options={{
-            headerShown: false,
-          }}
-          name="Login"
-          component={LoginScreen} />
-        <AuthStack.Screen
-          options={{
-            headerShown: false,
-          }}
-          name="Register"
-          component={RegistrationScreen} />
-       
-       </AuthStack.Navigator> 
- <StatusBar style="auto" />
-</NavigationContainer>
+    <NavigationContainer>
+      {routing}
+      <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
