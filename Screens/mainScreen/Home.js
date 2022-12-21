8d6491b91436d31f1667b5cddbcd646e1358594f
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-12-22 01:27:11 +0200

    hw-4/ add icons

--- Screens/mainScreen/Home.js
+++ Screens/mainScreen/Home.js
@@ -1,6 +1,10 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from "react";
-// import { StyleSheet } from "react-native";
+import { Feather } from '@expo/vector-icons'; 
+import { FontAwesome } from '@expo/vector-icons'; 
+import { Fontisto } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { StyleSheet } from "react-native";
+
 import PostsScreen from './PostsScreen';
 import CreatePostsScreen from './CreatePostsScreen';
 import ProfileScreen from './ProfileScreen';
@@ -9,24 +13,43 @@ const MainTab = createBottomTabNavigator();
 
 export const Home = () => {
   return (
-   <MainTab.Navigator>
+    <MainTab.Navigator screenOptions={{ tabBarShowLabel: false }}>
       <MainTab.Screen
         name="Posts"
-        component={PostsScreen} />
+        component={PostsScreen}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <Fontisto name="nav-icon-grid" size={26} color={color}/>
+          ),
+          headerRight: () => (
+            <AntDesign name="logout" size={24} color="#ff4500" style={styles.logout} />
+          ),
+        }}
+      />
       <MainTab.Screen
         name="Create"
-        component={CreatePostsScreen} />
+        component={CreatePostsScreen}
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+           <Feather name="plus-circle" size={42} color="#ff4500" />
+          ),
+        }}
+      />
       <MainTab.Screen
-        name="Profile"
-        component={ProfileScreen} />
+      name="Profile"
+      component={ProfileScreen}
+      options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <FontAwesome name="user" size={30} color={color} />
+          ),
+        }}
+      />
     </MainTab.Navigator>
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  logout: {
+  marginRight: 20,
+  },
+});
