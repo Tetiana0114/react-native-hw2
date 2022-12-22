c8d6435143bde6f025f9f798fd48aa2f76463443
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-12-23 00:59:11 +0200

    hw-5/add camera

--- App.js
+++ App.js
@@ -2,42 +2,42 @@ import { NavigationContainer } from "@react-navigation/native";
 import React, { useCallback, useEffect, useState } from 'react';
 import { } from "react-native";
 import { StatusBar } from 'expo-status-bar';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+// import * as Font from 'expo-font';
+// import * as SplashScreen from 'expo-splash-screen';
 import { useRoute } from './router';
 
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
+//   const [appIsReady, setAppIsReady] = useState(false);
   
-useEffect(() => {
-    async function prepare() {
-      try {
-      await Font.loadAsync({
-      "Roboto-Regular": require('./assets/fonts/Roboto-Regular.ttf'),
-      "Roboto-Medium": require('./assets/fonts/Roboto-Medium.ttf'),
-      "Roboto-Bold": require('./assets/fonts/Roboto-Bold.ttf'), 
-      });
-        await new Promise(resolve => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setAppIsReady(true);
-      }
-    }
-    prepare();
-}, []);
+// useEffect(() => {
+//     async function prepare() {
+//       try {
+//       await Font.loadAsync({
+//       "Roboto-Regular": require('./assets/fonts/Roboto-Regular.ttf'),
+//       "Roboto-Medium": require('./assets/fonts/Roboto-Medium.ttf'),
+//       "Roboto-Bold": require('./assets/fonts/Roboto-Bold.ttf'), 
+//       });
+//         await new Promise(resolve => setTimeout(resolve, 2000));
+//       } catch (e) {
+//         console.warn(e);
+//       } finally {
+//         setAppIsReady(true);
+//       }
+//     }
+//     prepare();
+// }, []);
 
-const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-    await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
+// const onLayoutRootView = useCallback(async () => {
+//     if (appIsReady) {
+//     await SplashScreen.hideAsync();
+//     }
+//   }, [appIsReady]);
 
-  if (!appIsReady) {
-    return null;
-  }
+//   if (!appIsReady) {
+//     return null;
+//   }
   
 const routing = useRoute({});
 // const routing = useRoute(null);
--- Screens/mainScreen/CreatePostsScreen.js
+++ Screens/mainScreen/CreatePostsScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Camera } from "expo-camera";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const CreatePostsScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>CreatePostsScreen</Text>
+      <Camera style={styles.camera}>
+         <TouchableOpacity onPress={() => {}} style={styles.snapContainer}>
+         <Text style={styles.snap}>SNAP</Text>
+      </TouchableOpacity>
+      </Camera>
     </View>
   );
 };
@@ -12,6 +18,23 @@ const CreatePostsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+   camera: {
+     height: 240,
+     marginTop: 32,
+     alignItems: "center",
+  },
+    snap: {
+    color: "#ff0000",
+  },
+  snapContainer: {
+    backgroundColor: "#fff",
+    marginTop: 90,
+    borderWidth: 1,
+    borderColor: "#fff",
+    width: 60,
+    height: 60,
+    borderRadius: 50,
     justifyContent: "center",
     alignItems: "center",
   },
