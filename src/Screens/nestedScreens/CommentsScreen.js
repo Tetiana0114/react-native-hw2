5aa0c00f61d56958275e421ddbd36be485710488
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2023-02-10 19:20:59 +0200

    hw-6/ getting comments

--- src/Screens/nestedScreens/CommentsScreen.js
+++ src/Screens/nestedScreens/CommentsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -9,17 +9,35 @@ import {
   Keyboard,
   KeyboardAvoidingView,
   Platform,
+  SafeAreaView,
+  FlatList,
 } from "react-native";
 import { useSelector } from "react-redux";
 import { fireStore } from "../../../firebase/config";
-import { doc, collection, addDoc } from "firebase/firestore";
+import { doc, collection, addDoc, onSnapshot } from "firebase/firestore";
 
 const CommentsScreen = ({ route }) => {
-  const { postId } = route.params;
   const [comment, setComment] = useState("");
-  const { login } = useSelector((state) => state.auth);
   const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [allComments, setAllComments] = useState([]);
+  const { postId } = route.params;
+  const { login } = useSelector((state) => state.auth);
   
+   useEffect(() => {
+    getUserComments();
+   }, []);
+  
+  const getUserComments = async () => {
+    try {
+    const post = doc(fireStore, "posts", postId);  
+    await onSnapshot(collection(post, "comments"), (data) => 
+    setAllComments(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    ); 
+    } catch (err) {
+    console.error(err);  
+    }
+  };
+   
   const addComment = async () => {
     const newComment = { comment, author: login };
     try {
@@ -34,7 +52,7 @@ const CommentsScreen = ({ route }) => {
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} style={styles.pressContainer}>
       <View style={styles.container}>
-         <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"}>
+        <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"}>
       <View>
         <TextInput
           style={styles.input}
@@ -47,7 +65,19 @@ const CommentsScreen = ({ route }) => {
       </View>
       <TouchableOpacity onPress={addComment} style={styles.addBtn}>
         <Text style={styles.addBtnName}>Add comment</Text>
-      </TouchableOpacity>
+          </TouchableOpacity>
+           <SafeAreaView>
+        <FlatList
+          data={allComments}
+          renderItem={({ item }) => (
+            <View style={styles.commentContainer}>
+              <Text style={styles.author}>{item.author}</Text>
+              <Text style={styles.authorText}>{item.comment}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
           </KeyboardAvoidingView>
       </View>
     </TouchableWithoutFeedback>
@@ -60,27 +90,43 @@ const styles = StyleSheet.create({
  },
   container: {
     flex: 1,
-    justifyContent: "center",
   },
   input: {
     fontSize: 18,
+    marginHorizontal: 16,
+    marginTop: 20,
+    marginBottom: 20,
+    padding: 10,
     borderBottomWidth: 2,
     borderBottomColor: "#E8E8E8",
-    padding: 10,
-    marginHorizontal: 16,
-    marginBottom: 30,
   },
   addBtn: {
-    marginHorizontal: 30,
-    padding: 12,
-    backgroundColor: "#FF6C00", 
     justifyContent: "center",
     alignItems: "center",
+    marginHorizontal: 30,
+    marginBottom: 20,
+    padding: 12,
+    backgroundColor: "#FF6C00", 
     borderRadius: 100,
   },
   addBtnName: {
     fontSize: 20,
     color: "#fff",
+  },
+  commentContainer: {
+    alignItems: "center",
+    marginHorizontal: 10,
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#20b2aa",
+    borderRadius: 100,
+  },
+  author: {
+    color: "#FF6C00",
+  },
+  authorText: {
+    fontSize: 18,
   }
 });
 
